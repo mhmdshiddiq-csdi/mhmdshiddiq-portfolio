f2d9c4c5dce9717cc49b9840560835071ea089df
mhmdshiddiq17 <muhammad.shiddiq@berijalan.net>
2025-06-25 15:03:58 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
+import { Routes, Route, useLocation } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { AnimatePresence } from "framer-motion";
+import GlobalStyle from "./globalStyles";
+import { lightTheme } from "./componets/Themes";
+
+//Components
+import Main from "./componets/Main"
+import AboutPage from "./componets/AboutPage"
+import BlogPage from "./componets/BlogPage"
+import WorkPage from "./componets/WorkPage"
+import MySkillsPage from "./componets/MySkillsPage"
+
 function App() {
-  return <div>
-    App File
-    </div>
-    
+  const location = useLocation();
+  return (
+    <>
+      <GlobalStyle />
+
+      <ThemeProvider theme={lightTheme}>
+        {/* <SoundBar /> */}
+
+        {/* For framer-motion animation on page change! */}
+        {/* Changed prop from exitBefore to mode */}
+        <AnimatePresence mode='wait'>
+          {/* Changed Switch to Routes */}
+
+          <Routes key={location.pathname} location={location} >
+            {/* Changed component to element */}
+
+            <Route path="/" element={<Main />} />
+
+            <Route path="/about" element={<AboutPage />} />
+
+            <Route path="/blog" element={<BlogPage />} />
+
+            <Route path="/work" element={<WorkPage />} />
+
+            <Route path="/skills" element={<MySkillsPage />} />
+            {/* Below is to catch all the other routes and send the user to main component,
+you can add custom 404 component or message instead of Main component*/}
+            <Route path="*" element={<Main />} />
+          </Routes>
+        </AnimatePresence>
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default App
-
+export default App;
---
+++ src/subComponents/PowerButton.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Power = styled.button`
+  position: fixed;
+  
+`
+
+const PowerButton = () => {
+  return (
+    <Power>
+      Home
+    </Power>
+  )
+}
+
+export default PowerButton
